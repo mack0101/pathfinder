871da6138640c8345e6bd90ba386ca48987327e7
Mitchell Backhouse <mitchell.backhouse+github@gmail.com>
2017-09-15 11:53:48 +1000

    added gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'mazes/index'
+  root to: 'mazes#index'
 
-  get 'mazes/new'
-
-  get 'mazes/edit'
-
-  get 'mazes/show'
+  resources :mazes
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
