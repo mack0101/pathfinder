1556c040e798459c7124f5e8f13740f8ea809635
Maciek K <mk@MaMBP.local>
2017-09-15 11:44:11 +1000

    maze and stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'mazes/index'
+
+  get 'mazes/new'
+
+  get 'mazes/edit'
+
+  get 'mazes/show'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
